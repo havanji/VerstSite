64a64367f008e97d5b118b23414acd31127b9fc7
Havanji <andriyparayko2017@gmail.com>
2022-12-05 17:07:20 +0200

    better root + dropDown.js + new interactive

---
+++ JS/dropDown.js
@@ -0,0 +1,19 @@
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches('.button')) {
+
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
